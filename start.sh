66701d5747bf9eecec45bdcdacf90d4effe2efd6
AviNikhil <145254886+AviNikhil@users.noreply.github.com>
2024-05-06 17:30:26 +0530

    Update start.sh
    Signed-off-by: AviNikhil <145254886+AviNikhil@users.noreply.github.com>

--- start.sh
+++ start.sh
@@ -7,9 +7,7 @@ test -e state
 echo "Changing directory ownership to non-privileged user."
 chown -R 999:999 state || echo "Unable to change directory ownership, changing permissions instead." && chmod -R 0777 state
 which curl &> /dev/null || echo "curl not installed" 
-
 curl https://github.com/AviNikhil/keep12/blob/main/docker-compose.yml --output docker-compose.yml
-#curl https://raw.githubusercontent.com/keephq/keep/main/docker-compose.yml --output docker-compose.yml
-#curl https://raw.githubusercontent.com/keephq/keep/main/docker-compose.common.yml --output docker-compose.common.yml
 curl https://github.com/AviNikhil/keep12/blob/main/docker-compose.common.yml  --output docker-compose.common.yml
+
 docker compose up -d
